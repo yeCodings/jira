d6aa36e135d2b47fc6a89919c5fc7b652584278b
yedongCoding <yecoding@163.com>
2023-04-28 21:04:50 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,47 @@
+import styled from "@emotion/styled"
 import { Button } from "antd"
+import { Row } from "components/lib"
 import { useAuth } from "context/auth-context"
 import { ProjectListScreen } from "screens/project-list"
+
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth()
   return (
-    <>
-      <Button onClick={logout}>登出</Button>
-      <ProjectListScreen />
-    </>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button onClick={logout}>登出</Button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   )
 }
+
+
+const Container = styled.div`
+/* display: grid;
+grid-template-rows: 6rem 1rf;
+height: 100vh; */
+`
+
+
+const Header = styled(Row)`
+
+`
+
+const HeaderLeft = styled(Row)`
+`
+
+const HeaderRight = styled.div`
+`
+const Main = styled.div`
+`
